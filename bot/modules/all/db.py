3e2edfca8e5263bb5e1d75fa251a32055e17468d
cojored <66964845+cojored@users.noreply.github.com>
2022-02-22 14:15:51 -0500

    Delete bot directory

--- bot/modules/all/db.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(
-    "mongodb+srv://cojored:conrad12@cluster0.u4l9r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
--- bot/modules/bot/db.py
+++
@@ -1,7 +0,0 @@
-from modules.all.db import client
-
-userdata = client['userdata']
-
-accounts = client['accounts']
-
-possibleAccounts = userdata['possibleAccounts']
